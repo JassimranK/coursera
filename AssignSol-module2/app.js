cba1eac8444c973178efad55e995ee281473d1c0
jassimran <just.simmi@gmail.com>
2017-02-11 00:06:42 -0800

    Two lists inside service

--- AssignSol-module2/app.js
+++ AssignSol-module2/app.js
@@ -1,16 +1,6 @@
 (function () {
   'use strict';
 
-  var myToBuyShoppingList= [
-    {itemName: "Milk", itemQuantity:"4"},
-    {itemName: "Eggs", itemQuantity:"24"},
-    {itemName: "Bread", itemQuantity:"1"},
-    {itemName: "Juice", itemQuantity:"2"},
-    {itemName: "Cookies", itemQuantity:"10"},
-  ];
-
-  var myBoughtShoppingList = [];
-
   angular.module('ShoppingListCheckOff',[])
   .controller('ToBuyController', ToBuyController)
   .controller('AlreadyBoughtController', AlreadyBoughtController)
@@ -19,7 +9,7 @@
   ToBuyController.$inject = ['ShoppingListCheckOffService'];
   function ToBuyController(ShoppingListCheckOffService){
     var toBuy = this;
-    toBuy.items= myToBuyShoppingList;
+    toBuy.items= ShoppingListCheckOffService.getMyToBuyShoppingList();
     toBuy.removeFromBought = function(itemName, itemQuantity, itemIndex){
       ShoppingListCheckOffService.removeFromBought(itemName, itemQuantity, itemIndex);
     }
@@ -28,16 +18,32 @@
   AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
   function AlreadyBoughtController(ShoppingListCheckOffService){
     var alreadyBought = this;
-    alreadyBought.items= myBoughtShoppingList;
+    alreadyBought.items= ShoppingListCheckOffService.getMyBoughtShoppingList();
   }
 
   //service
   function ShoppingListCheckOffService(){
     var service = this;
+    var myToBuyShoppingList= [
+      {itemName: "Milk", itemQuantity:"4"},
+      {itemName: "Eggs", itemQuantity:"24"},
+      {itemName: "Bread", itemQuantity:"1"},
+      {itemName: "Juice", itemQuantity:"2"},
+      {itemName: "Cookies", itemQuantity:"10"},
+    ];
+
+    var myBoughtShoppingList = [];
+
     service.removeFromBought = function (itemName, itemQuantity, itemIndex){
       var item = {name: itemName, quantity: itemQuantity};
       myBoughtShoppingList.push(item);
       myToBuyShoppingList.splice(itemIndex,1);
     }
+    service.getMyToBuyShoppingList = function () {
+      return myToBuyShoppingList;
+    };
+    service.getMyBoughtShoppingList = function () {
+      return myBoughtShoppingList;
+    };
   }
 })();
