f27b5fa69aa1cf448de60813dc254190ff288443
Jassimran <just.simmi@gmail.com>
2018-04-28 13:06:23 +0530

    Moving Ruby on Rails folder

--- Ruby on Rails/Course3- Ruby on Rails Web Services and Integration with MongoDB/fullstack-course3-module2/Advanced-MongoDB-Ruby-Driver/places/app/views/places/_place.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! place, :id, :formatted_address, :created_at, :updated_at
-json.url place_url(place, format: :json)
--- Ruby on Rails/Course3- Ruby on Rails Web Services and Integration with MongoDB/fullstack-course3-module3/Assignments/Lesson02-Mongoid-Relationships/contests/app/models/venue.rb
+++
@@ -1,7 +0,0 @@
-class Venue
-  include Mongoid::Document
-  field :name, type: String
-
-  embeds_one :address, as: :addressable, class_name: 'Address'
-  has_many :contests, dependent: :restrict
-end
